6696ec3110669ed66cb01deaab624d06ec75c2e8
Waleed Ahmad <34332682+Khushnu@users.noreply.github.com>
2026-09-01 21:21:28 +0500

    fix issue which was caused by some vibe coder

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,13 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Google Maps SDK must be given its key before any GMSMapView is created,
+    // otherwise it throws. Mirrors the com.google.android.geo.API_KEY
+    // meta-data entry in AndroidManifest.xml.
+    if let apiKey = Bundle.main.object(forInfoDictionaryKey: "GMSApiKey") as? String,
+       !apiKey.isEmpty {
+      GMSServices.provideAPIKey(apiKey)
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
